d46efee13618cbc9c5d5b1e664ccf20aa842e1c2
r0tif3r0 <gasertyf@gmail.com>
2025-01-19 15:06:45 +0300

    v.1

--- js/db/lampsDB.js
+++ js/db/lampsDB.js
@@ -3,48 +3,48 @@ export const lampsDB = [
       title: 'Встраиваемый светильник Markt',
       price: '5060',
       promoPrice: '3490',
-      poster: '../img/lamps_img/markt_lamp.png'
+      poster: 'img/lamps_img/markt_lamp.png'
     },
     {
       title: 'Линейный светильник ARG',
       price: '6700',
       promoPrice: '',
-      poster: '../img/lamps_img/arg_lamp.png'
+      poster: 'img/lamps_img/arg_lamp.png'
     },
     {
       title: 'Сведодиодный светильник',
       price: '6060',
       promoPrice: '5060',
-      poster: '../img/lamps_img/led_lamp.png'
+      poster: 'img/lamps_img/led_lamp.png'
     },
     {
       title: 'Встраиваемый светильник Markt',
       price: '3490',
       promoPrice: '',
-      poster: '../img/lamps_img/markt_lamp.png'
+      poster: 'img/lamps_img/markt_lamp.png'
     },
     {
       title: 'Линейный светильник ARG',
       price: '6060',
       promoPrice: '6700',
-      poster: '../img/lamps_img/arg_lamp.png'
+      poster: 'img/lamps_img/arg_lamp.png'
     },
     {
       title: 'Сведодиодный светильник',
       price: '5060',
       promoPrice: '',
-      poster: '../img/lamps_img/led_lamp.png'
+      poster: 'img/lamps_img/led_lamp.png'
     },
     {
       title: 'Встраиваемый светильник Markt',
       price: '6060',
       promoPrice: '3490',
-      poster: '../img/lamps_img/markt_lamp.png'
+      poster: 'img/lamps_img/markt_lamp.png'
     },
     {
       title: 'Линейный светильник ARG',
       price: '6700',
       promoPrice: '',
-      poster: '../img/lamps_img/arg_lamp.png'
+      poster: 'img/lamps_img/arg_lamp.png'
     },
   ]
